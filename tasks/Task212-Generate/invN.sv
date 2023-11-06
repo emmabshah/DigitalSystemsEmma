3dbc4819d2e284cb8d269639d798c32d1c840533
emmabshah <emma.shah@students.plymouth.ac.uk>
2023-11-06 10:28:21 +0000

    First set of labs attempted and start of second (216)

--- tasks/Task212-Generate/invN.sv
+++ tasks/Task212-Generate/invN.sv
@@ -1,14 +1,16 @@
 // Example of generate for to replicate hardware
-module invN #(parameter N=4) (output logic[N-1:0] Y, input logic[N-1:0] X);
+module invN #(parameter N=8) (output logic[N-1:0] Y, input logic[N-1:0] X);
 
 //An integer that will never be represented in the final synthesis
 genvar i;
 
 generate 
- for (i=0; i<N; i=i+2) 
+ for (i=0; i<4; i=i+2) 
  begin : i_loop1
 		assign Y [i] = ~X [i];
- end
+
+end
+ 
 endgenerate
 
 endmodule
--- tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
+++ tasks/Task215-2-to-1-multiplexer/mux2_structural.sv
@@ -1,6 +1,15 @@
 module mux2_structural (output logic y, input logic a, b, s);
 
 // Write HDL here
+//Internal Wires
+wire term0, term3, not_s;
 
+//Continuous Assignment (order does not matter)
+assign not_s = ~s;
+assign term0 = not_s & a;
+assign term3 = s & b;
+assign y = term0 | term3;
 
 endmodule
+
+
